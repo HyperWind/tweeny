2ca583f5cb9c523051ff3060b388446c4c3aa969
Karolis Lasys (HyperWind) <karolioofficial@outlook.com>
2015-06-16 23:19:33 +0300

    made it accept only valid hashtags

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,7 +4,27 @@ angular
 	.module('tweenyApp')
 	.controller('MainCtrl', function ($scope, $http) {
 
+    function queryValid(query) {
+
+      var ret;
+
+      if (query.length > 0) {
+        ret = query.replace(/(#|\w+|\d+|\s+|_)/g, '').length === 0;
+      } else {
+        ret = false;
+      }
+
+      return ret;
+
+    }
+
     function search(query) {
+
+      if (!queryValid(query)) {
+        alert("Invalid query!");
+        return;
+      }
+
       $scope.isSearching = true;
       $http.get('http://127.0.0.1:8000/twitter/' + query.replace(/[#\s]/g, '')).then(function (result) {
         $scope.tweets = result.data.statuses.map(function (tweet) {
@@ -24,5 +44,6 @@ angular
     $scope.query = '#cats';
     $scope.isSearching = false;
     $scope.search = search;
+    $scope.queryValid = queryValid;
 
   });
