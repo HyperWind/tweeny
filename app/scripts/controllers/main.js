02f9f4b41d608aeea2eb94210528ad4867134f39
Karolis Lasys (HyperWind) <karolioofficial@outlook.com>
2015-06-18 09:35:10 +0300

    adding in localstorage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,76 @@
 'use strict';
 
+function Storage() {
+
+  this.container = window.localStorage;
+
+}
+
+Storage.prototype.push = function(query) {
+
+  this.container.setItem(this.container.length.toString() + '_tweeny', query);
+
+};
+
+Storage.prototype.at = function(index) {
+
+  if (index + 1 > this.size && index < 0) {
+
+    return {err: 'exceeds container bounds', cont: null};
+
+  } else {
+
+    return {err: null, cont: this.container.getItem(index.toString() + '_tweeny')};
+
+  }
+
+};
+
+function unique(arr) {
+
+  var which = 0;
+  var uniqueArr = arr.map(function (val) {
+
+    for (var i = 0; i < arr.length; i++) {
+
+      if (val === arr[i] && i !== which) delete arr[i];
+
+    }
+
+    which++;
+
+  }).filter(function (val) {
+
+    return val != undefined;
+
+  });
+
+  return uniqueArr;
+}
+
 angular
 	.module('tweenyApp')
-	.controller('MainCtrl', function ($scope, $http) {
+	.controller('MainCtrl', function ($scope, $http) {  
+
+    function getHistory(iters, all) {
+
+      var amount = all !== true ? iters : $scope.storage.container.length;
+      var historyArr = [];
+      var current;
+
+        for (var i = $scope.storage.container.length - amount; i < $scope.storage.container.length; i++) {
+
+          current = $scope.storage.at(i);
+
+          if (current.err === null) {
+              historyArr.push(current.cont);
+          }
+
+        }
+
+      return unique(historyArr);
+
+    }
 
     function search(query) {
 
@@ -13,6 +81,7 @@ angular
 
       }
 
+      $scope.storage.push(query);
       $scope.badclick = false;
       $scope.isSearching = true;
       $http.get('http://127.0.0.1:8000/twitter/' + query.replace(/[#\s]/g, '')).then(function (result) {
@@ -35,5 +104,7 @@ angular
     $scope.search = search;
     $scope.searchForm = {};
     $scope.badclick = false;
+    $scope.getHistory = getHistory;
+    $scope.storage = new Storage();
 
   });
